cf8d0e3977e74c6712b5284f5b7a89a111dea311
SJY <jihoseo2008@gmail.com>
2024-12-18 12:04:42 +0900

    feat :: signup page

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Start from "./pages/start"
 import Login from "./pages/login"
+import Signup from "./pages/signup/signup"
+import Inform from "./pages/signup/inform"
 
 function Router() {
     return (
@@ -8,6 +10,8 @@ function Router() {
             <Routes>
                 <Route path="/" element={<Start />} />
                 <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<Signup />} />
+                <Route path="/inform" element={<Inform />} />
             </Routes>
         </BrowserRouter>
     )
