58ef7ad2341c9e81758970d8af83f0b5e10994a3
SJY <jihoseo2008@gmail.com>
2024-12-18 20:26:14 +0900

    feat :: header
    add header components

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,6 +3,7 @@ import Start from "./pages/start"
 import Login from "./pages/login"
 import Signup from "./pages/signup/signup"
 import Inform from "./pages/signup/inform"
+import Main from "./pages/main"
 
 function Router() {
     return (
@@ -12,6 +13,7 @@ function Router() {
                 <Route path="/login" element={<Login />} />
                 <Route path="/signup" element={<Signup />} />
                 <Route path="/inform" element={<Inform />} />
+                <Route path="/main" element={<Main />} />
             </Routes>
         </BrowserRouter>
     )
---
+++ src/pages/main.tsx
@@ -0,0 +1,11 @@
+import Header from "../components/header/header"
+
+function Main() {
+    return (
+        <>
+            <Header />
+        </>
+    )
+}
+
+export default Main
