accf9a1454ecbfb680fc77d41e8d04cdb1a122d9
SJY <jihoseo2008@gmail.com>
2025-01-08 21:02:38 +0900

    docs :: background
    add a background component
    and edit page(use component)

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,4 +1,3 @@
-import Header from "../components/common/header"
 import TimeTable from "../components/timtable"
 import styled from "styled-components"
 import { Colors } from "../style/colors"
@@ -6,104 +5,48 @@ import { LuNotebookPen } from "react-icons/lu"
 import { GrNotes } from "react-icons/gr"
 import { BsPersonCircle } from "react-icons/bs"
 import { useNavigate } from "react-router-dom"
+import Background from "../components/background"
 
 function Main() {
     const navigate = useNavigate()
 
     return (
         <>
-            <Header />
-            <Container>
-                <TopContainer>
-                    <Title>홈</Title>
-                    <SubTitle>홈에서 종합적으로 확인하고 관리하세요</SubTitle>
-                </TopContainer>
+            <Background
+                title="홈"
+                subtitle="홈에서 종합적으로 확인하고 관리하세요"
+            >
+                <ButtonContainer>
+                    <ButtonWrapper>
+                        <Button>
+                            <LuNotebookPen />
+                        </Button>
+                        일정관리
+                    </ButtonWrapper>
+                    <ButtonWrapper>
+                        <Button>
+                            <GrNotes />
+                        </Button>
+                        성적기록
+                    </ButtonWrapper>
+                    <ButtonWrapper>
+                        <Button onClick={() => navigate("/mypage")}>
+                            <BsPersonCircle />
+                        </Button>
+                        마이페이지
+                    </ButtonWrapper>
+                </ButtonContainer>
 
-                <BottomContainer>
-                    <Wrapper>
-                        <ButtonContainer>
-                            <ButtonWrapper>
-                                <Button>
-                                    <LuNotebookPen />
-                                </Button>
-                                일정관리
-                            </ButtonWrapper>
-                            <ButtonWrapper>
-                                <Button>
-                                    <GrNotes />
-                                </Button>
-                                성적기록
-                            </ButtonWrapper>
-                            <ButtonWrapper>
-                                <Button onClick={() => navigate("/mypage")}>
-                                    <BsPersonCircle />
-                                </Button>
-                                마이페이지
-                            </ButtonWrapper>
-                        </ButtonContainer>
-
-                        <TimeTableContainer>
-                            <TimeTableTitle>이번 주 시간표</TimeTableTitle>
-                            <TimeTable />
-                        </TimeTableContainer>
-                    </Wrapper>
-                </BottomContainer>
-            </Container>
+                <TimeTableContainer>
+                    <TimeTableTitle>이번 주 시간표</TimeTableTitle>
+                    <TimeTable />
+                </TimeTableContainer>
+            </Background>
         </>
     )
 }
 
 export default Main
-
-const Container = styled.div`
-    width: 100%;
-    min-height: 100vh;
-    background: ${Colors.White};
-    display: flex;
-    justify-content: start;
-    align-items: center;
-    flex-direction: column;
-`
-
-const TopContainer = styled.div`
-    width: 100%;
-    height: 25vh;
-    display: flex;
-    justify-content: center;
-    align-items: start;
-    flex-direction: column;
-    gap: 10px;
-`
-
-const Title = styled.div`
-    margin-top: 40px;
-    margin-left: 25%;
-    font-size: 24px;
-    font-weight: 800;
-    color: ${Colors.Black};
-`
-
-const SubTitle = styled.div`
-    margin-left: 25%;
-    font-size: 16px;
-    font-weight: 500;
-    color: ${Colors.Gray500};
-`
-
-const BottomContainer = styled.div`
-    width: 100%;
-    min-height: 75vh;
-    background-color: ${Colors.Orange100};
-`
-
-const Wrapper = styled.div`
-    padding-top: 30px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    gap: 50px;
-`
 
 const ButtonContainer = styled.div`
     display: flex;
--- src/pages/mypage.tsx
+++ src/pages/mypage.tsx
@@ -1,115 +1,55 @@
 import styled from "styled-components"
 import { Colors } from "../style/colors"
-import Header from "../components/common/header"
 import Input from "../components/common/input"
 import Button from "../components/common/button"
+import Background from "../components/background"
 
 function Mypage() {
     return (
         <>
-            <Header />
-            <Container>
-                <TopContainer>
-                    <Title>마이페이지</Title>
-                    <SubTitle>이 곳에서 자신의 정보를 수정하세요</SubTitle>
-                </TopContainer>
+            <Background
+                title="마이페이지"
+                subtitle="이 곳에서 자신의 정보를 수정하세요"
+            >
+                <Form>
+                    <Input
+                        label="이름"
+                        placeholder="이름을 수정해주세요"
+                        id="name"
+                        width={450}
+                    />
+                    <Input
+                        label="학년"
+                        placeholder="학년을 수정해주세요"
+                        id="grade"
+                        type="number"
+                        width={450}
+                        max={3}
+                    />
+                    <Input
+                        label="반"
+                        placeholder="반을 수정해주세요"
+                        id="class"
+                        type="number "
+                        width={450}
+                        max={4}
+                    />
+                    <ButtonWrapper>
+                        <Cancel>되돌리기</Cancel>
+                        <Button width={125}>수정하기</Button>
+                    </ButtonWrapper>
+                </Form>
 
-                <BottomContainer>
-                    <Wrapper>
-                        <Form>
-                            <Input
-                                label="이름"
-                                placeholder="이름을 수정해주세요"
-                                id="name"
-                                width={450}
-                            />
-                            <Input
-                                label="학년"
-                                placeholder="학년을 수정해주세요"
-                                id="grade"
-                                type="number"
-                                width={450}
-                                max={3}
-                            />
-                            <Input
-                                label="반"
-                                placeholder="반을 수정해주세요"
-                                id="class"
-                                type="number "
-                                width={450}
-                                max={4}
-                            />
-                            <ButtonWrapper>
-                                <Cancel>되돌리기</Cancel>
-                                <Button width={125}>수정하기</Button>
-                            </ButtonWrapper>
-                        </Form>
-
-                        <RemoveContainer>
-                            계정 삭제 시 프로필 및 기록 내용이 삭제됩니다.
-                            <RemoveButton>계정삭제</RemoveButton>
-                        </RemoveContainer>
-                    </Wrapper>
-                </BottomContainer>
-            </Container>
+                <RemoveContainer>
+                    계정 삭제 시 프로필 및 기록 내용이 삭제됩니다.
+                    <RemoveButton>계정삭제</RemoveButton>
+                </RemoveContainer>
+            </Background>
         </>
     )
 }
 
 export default Mypage
-
-const Container = styled.div`
-    width: 100%;
-    min-height: 100vh;
-    background: ${Colors.White};
-    display: flex;
-    justify-content: start;
-    align-items: center;
-    flex-direction: column;
-`
-
-const TopContainer = styled.div`
-    width: 100%;
-    height: 25vh;
-    display: flex;
-    justify-content: center;
-    align-items: start;
-    flex-direction: column;
-    gap: 10px;
-`
-
-const Title = styled.div`
-    margin-top: 40px;
-    margin-left: 25%;
-    font-size: 24px;
-    font-weight: 800;
-    color: ${Colors.Black};
-`
-
-const SubTitle = styled.div`
-    margin-left: 25%;
-    font-size: 16px;
-    font-weight: 500;
-    color: ${Colors.Gray500};
-`
-
-const BottomContainer = styled.div`
-    width: 100%;
-    min-height: 75vh;
-    background-color: ${Colors.Orange100};
-    display: flex;
-    justify-content: center;
-    align-items: start;
-`
-
-const Wrapper = styled.div`
-    padding-top: 30px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    gap: 50px;
-`
 
 const Form = styled.div`
     width: 500px;
