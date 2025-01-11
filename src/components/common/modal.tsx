52d9b2a8c97fe9e943d55f2f70c63fce28cdaae4
SJY <jihoseo2008@gmail.com>
2025-01-11 17:19:22 +0900

    feat :: modal component
    add modal component
    set cancel account modal

---
+++ src/components/common/modal.tsx
@@ -0,0 +1,149 @@
+import styled from "styled-components"
+import { Colors } from "../../style/colors"
+import { IoClose } from "react-icons/io5"
+
+interface Props {
+    title?: string
+    content?: string
+    check?: () => void
+    close?: () => void
+}
+
+function Modal({ title, content, check, close }: Props) {
+    return (
+        <>
+            <Background>
+                <Container>
+                    <TopContainer>
+                        <Title>{title}</Title>
+                        <CloseButton onClick={close}>
+                            <IoClose />
+                        </CloseButton>
+                    </TopContainer>
+
+                    <BottomContainer>
+                        <Content>{content}</Content>
+                    </BottomContainer>
+
+                    <ButtonContainer>
+                        <CancelButton onClick={close}>취소</CancelButton>
+                        <CheckButton onClick={check}>확인</CheckButton>
+                    </ButtonContainer>
+                </Container>
+            </Background>
+        </>
+    )
+}
+
+export default Modal
+
+const Background = styled.div`
+    position: fixed;
+    background: #00000060;
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+const Container = styled.div`
+    width: 40%;
+    height: 45%;
+    border-radius: 20px;
+    background-color: ${Colors.White};
+    display: flex;
+    justify-content: start;
+    align-items: center;
+    flex-direction: column;
+`
+
+const TopContainer = styled.div`
+    height: 20%;
+    width: 95%;
+    border-bottom: 1px solid ${Colors.Gray300};
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+const Title = styled.p`
+    margin-left: 10px;
+    font-size: 22px;
+    color: ${Colors.Black};
+    font-weight: 600;
+`
+
+const CloseButton = styled.div`
+    width: 40px;
+    height: 40px;
+    font-size: 30px;
+    margin-left: auto;
+    margin-right: 5px;
+    color: ${Colors.Gray500};
+    cursor: pointer;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+const BottomContainer = styled.div`
+    width: 95%;
+    height: 60%;
+`
+
+const Content = styled.p`
+    margin-left: 10px;
+    margin-top: 15px;
+    font-size: 20px;
+    font-weight: 500;
+    white-space: pre-line;
+    line-height: 30px;
+    color: ${Colors.Gray600};
+`
+
+const ButtonContainer = styled.div`
+    width: 95%;
+    height: 20%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+`
+
+const ButtonWrapper = styled.div`
+    margin-left: auto;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+`
+
+const CancelButton = styled.button`
+    margin-left: auto;
+    width: 100px;
+    height: 40px;
+    border-radius: 23px;
+    border: 2px solid ${Colors.Gray500};
+    background-color: ${Colors.White};
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: ${Colors.Gray500};
+    font-size: 16px;
+    cursor: pointer;
+`
+
+const CheckButton = styled.button`
+    width: 100px;
+    height: 40px;
+    border-radius: 23px;
+    border: none;
+    background-color: ${Colors.Orange400};
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: ${Colors.White};
+    font-size: 16px;
+    cursor: pointer;
+`
